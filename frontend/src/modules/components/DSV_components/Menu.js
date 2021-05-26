ab3483dd91fdc09f64d2b8dde710e0912f4f4bba
hu0810 <luelue880810@gmail.com>
2021-05-26 23:37:30 +0800

    Tree visualization completed

---
+++ frontend/src/modules/components/DSV_components/Menu.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function Menu(props) {
+	return (
+		<>
+			<div id="bst">
+				<button onClick={() => props.selector('bst')}>
+					Binary Search Tree
+				</button>
+			</div>
+
+			<div id="trie">
+				<button onClick={() => props.selector('trie')}>Trie</button>
+			</div>
+
+			<div id="avl">
+				<button onClick={() => props.selector('avl')}>
+					AVL Tree
+				</button>
+			</div>
+
+			<div id="minheap">
+				<button onClick={() => props.selector('minheap')}>
+					Min Heap
+				</button>
+			</div>
+
+			<div id="maxheap">
+				<button onClick={() => props.selector('maxheap')}>
+					Max Heap
+				</button>
+			</div>
+
+			<div id="redb">
+				<button onClick={() => props.selector('redb')}>
+					Red Black Tree
+				</button>
+			</div>
+		</>
+	)
+}
+
+export default Menu
